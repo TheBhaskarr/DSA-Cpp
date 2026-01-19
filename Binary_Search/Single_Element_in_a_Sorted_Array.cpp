d80ee11de2fde9bcb3a53081ad6ab97f7a93c93a
Bhaskar <bhaskarmishra992@gmail.com>
2026-01-20 00:05:32 +0530

    Add Single Element in a Sorted Array

---
+++ Binary_Search/Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& A) {
+        int n = A.size();
+        if (n == 1) return A[0];
+
+        // edge cases
+        if (A[0] != A[1]) return A[0];
+        if (A[n-1] != A[n-2]) return A[n-1];
+
+        int st = 1, end = n - 2;
+
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if (A[mid-1] != A[mid] && A[mid] != A[mid+1])
+                return A[mid];
+
+            if (mid % 2 == 0) { // even
+                if (A[mid] == A[mid-1]) end = mid - 1;
+                else st = mid + 1;
+            } else { // odd
+                if (A[mid] == A[mid-1]) st = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
