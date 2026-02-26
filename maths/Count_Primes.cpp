dd5af0f458bf129b0e62c05e7033e9536c938266
Bhaskar <bhaskarmishra992@gmail.com>
2026-02-26 15:06:43 +0530

    Add Count Primes (Sieve of Eratosthenes)

---
+++ maths/Count_Primes.cpp
@@ -0,0 +1,27 @@
+// Problem: Count Primes (LeetCode 204)
+// Approach: Sieve of Eratosthenes
+// Time Complexity: O(n log log n)
+// Space Complexity: O(n)
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector <bool> isPrime(n+1, true);
+        int count =0;
+
+        for(int i=2; i<n; i++){
+            if(isPrime[i]){
+                count++;
+
+                for(int j= i*2; j<n; j=j+i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        return count;
+
+    }
+};
